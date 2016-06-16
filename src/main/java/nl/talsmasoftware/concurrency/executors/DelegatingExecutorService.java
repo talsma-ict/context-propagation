4625fe9100bd2b31afe90c52d5fac1152df63ae1
Sjoerd Talsma <talsma.s@tkppensioen.nl>
2016-06-16 16:14:47 +0200

    Added two initial packages and two abstract delegating classes for: futures and executors.

---
+++ src/main/java/nl/talsmasoftware/concurrency/executors/DelegatingExecutorService.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2016 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package nl.talsmasoftware.concurrency.executors;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * Abstract baseclass that makes it a little easier to wrap existing {@link ExecutorService} implementations by
+ * forwarding all methods to a <code>delegate</code> executor service.
+ * <p>
+ * Although this class implements all required methods of {@link ExecutorService} it is still declared as an
+ * <em>abstract</em> class. This is because it does not provide any value in itself.
+ *
+ * @author <a href="mailto:info@talsma-software.nl">Sjoerd Talsma</a>
+ */
+public abstract class DelegatingExecutorService implements ExecutorService {
+
+    /**
+     * Subclass-accessible reference to the delegate executor service being wrapped.
+     */
+    protected final ExecutorService delegate;
+
+    protected DelegatingExecutorService(ExecutorService delegate) {
+        if (delegate == null) throw new IllegalArgumentException("No delegate executor service provided!");
+        this.delegate = delegate;
+    }
+
+    public void shutdown() {
+        delegate.shutdown();
+    }
+
+    public List<Runnable> shutdownNow() {
+        return delegate.shutdownNow();
+    }
+
+    public boolean isShutdown() {
+        return delegate.isShutdown();
+    }
+
+    public boolean isTerminated() {
+        return delegate.isTerminated();
+    }
+
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return delegate.awaitTermination(timeout, unit);
+    }
+
+    public <T> Future<T> submit(Callable<T> task) {
+        return delegate.submit(task);
+    }
+
+    public <T> Future<T> submit(Runnable task, T result) {
+        return delegate.submit(task, result);
+    }
+
+    public Future<?> submit(Runnable task) {
+        return delegate.submit(task);
+    }
+
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        return delegate.invokeAll(tasks);
+    }
+
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+        return delegate.invokeAll(tasks, timeout, unit);
+    }
+
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+        return delegate.invokeAny(tasks);
+    }
+
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return delegate.invokeAny(tasks, timeout, unit);
+    }
+
+    public void execute(Runnable command) {
+        delegate.execute(command);
+    }
+
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this == other || (other != null && getClass().equals(other.getClass())
+                && delegate.equals(((DelegatingExecutorService) other).delegate));
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + '{' + delegate + '}';
+    }
+
+}
---
+++ src/main/java/nl/talsmasoftware/concurrency/futures/DelegatingFuture.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2016 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package nl.talsmasoftware.concurrency.futures;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Abstract baseclass that simplifies wrapping an existing {@link Future} by forwarding all required methods to a
+ * delegate future object.
+ * <p>
+ * Although this class implements all required methods of {@link Future} it is still declared as an
+ * <em>abstract</em> class. This is because it does not provide any value in itself.
+ *
+ * @author <a href="mailto:info@talsma-software.nl">Sjoerd Talsma</a>
+ */
+public abstract class DelegatingFuture<V> implements Future<V> {
+
+    protected final Future<V> delegate;
+
+    protected DelegatingFuture(Future<V> delegate) {
+        if (delegate == null) throw new IllegalArgumentException("No delegate future provided!");
+        this.delegate = delegate;
+    }
+
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        return delegate.cancel(mayInterruptIfRunning);
+    }
+
+    public boolean isCancelled() {
+        return delegate.isCancelled();
+    }
+
+    public boolean isDone() {
+        return delegate.isDone();
+    }
+
+    public V get() throws InterruptedException, ExecutionException {
+        return delegate.get();
+    }
+
+    public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return delegate.get(timeout, unit);
+    }
+
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this == other || (other != null && getClass().equals(other.getClass())
+                && delegate.equals(((DelegatingFuture) other).delegate));
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + '{' + delegate + '}';
+    }
+
+}
