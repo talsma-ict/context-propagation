2a738a45f3fbd9a00c16c3cde6f27d856d5d1a76
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2016-06-17 12:59:03 +0200

    Added pretty trivial unit test to DelegatingFuture.

--- src/test/java/nl/talsmasoftware/concurrency/executors/DelegatingExecutorServiceTest.java
+++ src/test/java/nl/talsmasoftware/concurrency/executors/DelegatingExecutorServiceTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package nl.talsmasoftware.concurrency.executors;
 
 import org.junit.After;
@@ -42,6 +59,11 @@ public class DelegatingExecutorServiceTest {
     @After
     public void noMoreInteractions() {
         verifyNoMoreInteractions(delegate);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullConstructor() {
+        new TestDelegatingExecutorService(null);
     }
 
     @Test
