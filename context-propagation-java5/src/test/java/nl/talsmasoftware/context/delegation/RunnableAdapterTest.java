43999ec7410bddeb91317bee1beff8f32eb9efd9
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2016-12-08 19:47:18 +0100

    Moved all code into a 'java5' subdirectory and created a maven project module for it.

--- context-propagation-java5/src/test/java/nl/talsmasoftware/context/delegation/RunnableAdapterTest.java
+++ context-propagation-java5/src/test/java/nl/talsmasoftware/context/delegation/RunnableAdapterTest.java
@@ -17,7 +17,6 @@
 
 package nl.talsmasoftware.context.delegation;
 
-import nl.talsmasoftware.context.delegation.RunnableAdapter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
