a9169d17203add2e972fb1ac816e5bd022e9e5ae
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-05-16 21:32:38 +0200

    Initial dabbling with ActiveSpanManager delegating to GlobalTracer.

---
+++ opentracing-span-propagation/src/main/java/nl/talsmasoftware/context/opentracing/ActiveSpanManager.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016-2017 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.context.opentracing;
+
+import io.opentracing.ActiveSpan;
+import io.opentracing.util.GlobalTracer;
+import nl.talsmasoftware.context.Context;
+import nl.talsmasoftware.context.ContextManager;
+
+/**
+ * @author Sjoerd Talsma
+ */
+public class ActiveSpanManager implements ContextManager<ActiveSpan.Continuation> {
+
+    @Override
+    public Context<ActiveSpan.Continuation> getActiveContext() {
+        final ActiveSpan activeSpan = GlobalTracer.get().activeSpan();
+        return new Context<ActiveSpan.Continuation>() {
+            @Override
+            public ActiveSpan.Continuation getValue() {
+                return activeSpan == null ? null : activeSpan.capture();
+            }
+
+            @Override
+            public void close() {
+                if (activeSpan != null) activeSpan.close();
+            }
+        };
+    }
+
+    @Override
+    public Context<ActiveSpan.Continuation> initializeNewContext(final ActiveSpan.Continuation continuation) {
+        final ActiveSpan currentActiveSpan = GlobalTracer.get().activeSpan();
+        final ActiveSpan activated = continuation.activate();
+
+        return new Context<ActiveSpan.Continuation>() {
+            @Override
+            public ActiveSpan.Continuation getValue() {
+                return activated != null ? activated.capture() : null;
+            }
+
+            @Override
+            public void close() {
+                if (activated != null) activated.deactivate();
+            }
+        };
+    }
+
+}
---
+++ opentracing-span-propagation/src/test/java/nl/talsmasoftware/context/opentracing/ActiveSpanManagerTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2016-2017 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.context.opentracing;
+
+import io.opentracing.ActiveSpan;
+import io.opentracing.mock.MockSpan;
+import io.opentracing.mock.MockTracer;
+import io.opentracing.util.GlobalTracer;
+import io.opentracing.util.ThreadLocalActiveSpanSource;
+import nl.talsmasoftware.context.executors.ContextAwareExecutorService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyCollectionOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Sjoerd Talsma
+ */
+public class ActiveSpanManagerTest {
+    static final ThreadLocalActiveSpanSource TL_ACTIVE_SPAN_SOURCE = new ThreadLocalActiveSpanSource();
+
+    MockTracer mockTracer;
+    ExecutorService threadpool;
+
+    @Before
+    public void registerMockGlobalTracer() {
+        assertThat("Pre-existing GlobalTracer", GlobalTracer.isRegistered(), is(false));
+        GlobalTracer.register(mockTracer = new MockTracer(TL_ACTIVE_SPAN_SOURCE));
+        threadpool = new ContextAwareExecutorService(Executors.newCachedThreadPool());
+    }
+
+    @After
+    public void shutdownThreadpool() {
+        threadpool.shutdown();
+    }
+
+    @After
+    public void clearGlobalTracer() throws NoSuchFieldException, IllegalAccessException {
+        Field tracer = GlobalTracer.class.getDeclaredField("tracer");
+        tracer.setAccessible(true);
+        tracer.set(null, null);
+        tracer.setAccessible(false);
+    }
+
+    private static final Callable<String> GET_BAGGAGE_ITEM = new Callable<String>() {
+        @Override
+        public String call() {
+            ActiveSpan activeSpan = GlobalTracer.get().activeSpan();
+            return activeSpan == null ? "no-active-span" : activeSpan.getBaggageItem("baggage-item");
+        }
+
+    };
+
+    @Test
+    public void testSingleSnapshotInCurrentThread() throws Exception {
+        ActiveSpan outerSpan = mockTracer.buildSpan("first-op").startActive();
+        outerSpan.setBaggageItem("baggage-item", "in-outer-span");
+
+        // sanity-check: outerSpan should be the active span..
+        assertThat("sanity-check", GET_BAGGAGE_ITEM.call(), equalTo("in-outer-span"));
+
+        // The active span reference should propagate to the background thread and therefore return the baggage.
+        Future<String> backgroundBaggage = threadpool.submit(GET_BAGGAGE_ITEM);
+        assertThat("background baggage", backgroundBaggage.get(), equalTo("in-outer-span"));
+        assertThat("span finished?", mockTracer.finishedSpans(), is(emptyCollectionOf(MockSpan.class)));
+
+        outerSpan.close();
+        assertThat("span finished?", mockTracer.finishedSpans(), hasSize(1));
+        assertThat("baggage of active span", GET_BAGGAGE_ITEM.call(), equalTo("no-active-span"));
+    }
+
+}
