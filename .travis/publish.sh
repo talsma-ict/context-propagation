1732438fde03e43dc859befbe4db07bf82b2f211
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-11-07 19:10:53 +0100

    Move travis config files to the .travis subdir.

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -110,7 +110,7 @@ if ! is_pull_request && build_started_by_tag; then
   check_release_tag
 fi
 
-./mvnw verify -nsu
+./mvnw -nsu verify
 
 # If we are on a pull request, our only job is to run tests, which happened above via ./mvnw install
 if is_pull_request; then
@@ -118,10 +118,10 @@ if is_pull_request; then
 # If we are on master, we will deploy the latest snapshot or release version
 #   - If a release commit fails to deploy for a transient reason, delete the broken version from bintray and click rebuild
 elif is_travis_branch_master; then
-  ./mvnw --batch-mode -s .mvn/settings.xml -Prelease -nsu -DskipTests deploy
+  ./mvnw --batch-mode -Prelease -nsu -DskipTests deploy
 
 # If we are on a release tag, the following will update any version references and push a version tag for deployment.
 elif build_started_by_tag; then
   safe_checkout_master
-  ./mvnw --batch-mode -s .mvn/settings.xml -Prelease -nsu -DreleaseVersion="$(release_version)" -Darguments="-DskipTests" release:prepare
+  ./mvnw --batch-mode -Prelease -nsu -DreleaseVersion="$(release_version)" -Darguments="-DskipTests" release:prepare
 fi
