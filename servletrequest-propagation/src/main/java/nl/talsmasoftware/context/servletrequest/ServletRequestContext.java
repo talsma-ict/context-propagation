28b88ad58263fbdbb4967cd57ff12e4d290868c1
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-01-03 14:44:33 +0100

    Introduce a module to maintain a current Locale.

--- servletrequest-propagation/src/main/java/nl/talsmasoftware/context/servletrequest/ServletRequestContext.java
+++ servletrequest-propagation/src/main/java/nl/talsmasoftware/context/servletrequest/ServletRequestContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 Talsma ICT
+ * Copyright 2016-2018 Talsma ICT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ final class ServletRequestContext extends AbstractThreadLocalContext<ServletRequ
     }
 
     /**
-     * Dummy context containing.
+     * Dummy context containing no servlet request.
      */
     private static final class DummyContext implements Context<ServletRequest> {
         private static final DummyContext INSTANCE = new DummyContext();
