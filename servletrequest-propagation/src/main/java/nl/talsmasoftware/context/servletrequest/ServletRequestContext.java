7674e53b9bc08bc7bc5ed7f01462af7554190188
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-10-24 14:11:51 +0200

    Add Clearable support to ServletRequestContext and remove stack
    There can now be at most 1 ServletRequest context in each thread which
    should be enough for normal use cases.
    
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

--- servletrequest-propagation/src/main/java/nl/talsmasoftware/context/servletrequest/ServletRequestContext.java
+++ servletrequest-propagation/src/main/java/nl/talsmasoftware/context/servletrequest/ServletRequestContext.java
@@ -16,76 +16,36 @@
 package nl.talsmasoftware.context.servletrequest;
 
 import nl.talsmasoftware.context.Context;
-import nl.talsmasoftware.context.threadlocal.AbstractThreadLocalContext;
 
 import javax.servlet.ServletRequest;
-import java.util.logging.Logger;
 
 /**
- * ThreadLocal context containing the current {@link ServletRequest}.
+ * Simple (unstacked) context using a Threadlocal containing the current {@link ServletRequest}.
  *
  * @author Sjoerd Talsma
  */
-final class ServletRequestContext extends AbstractThreadLocalContext<ServletRequest> {
-    private static final Logger LOGGER = Logger.getLogger(ServletRequestContext.class.getName());
+final class ServletRequestContext implements Context<ServletRequest> {
 
-    /**
-     * The ThreadLocal context containing the {@link ServletRequest}.
-     */
-    private static final ThreadLocal<ServletRequestContext> CONTEXT = threadLocalInstanceOf(ServletRequestContext.class);
+    private static final ThreadLocal<ServletRequestContext> CONTEXT = new ThreadLocal<ServletRequestContext>();
 
-    /**
-     * Creates a new context with the specified request.
-     * <p>
-     * The new context will be made the active context for the current thread.
-     *
-     * @param newValue The new value to become active in this new context
-     *                 (or <code>null</code> to register a new context with 'no value').
-     */
-    ServletRequestContext(ServletRequest newValue) {
-        super(newValue);
+    volatile ServletRequest servletRequest;
+
+    ServletRequestContext(ServletRequest servletRequest) {
+        this.servletRequest = servletRequest;
+        CONTEXT.set(this);
     }
 
-    /**
-     * The current servlet request context.
-     * <p>
-     * If no active {@link ServletRequest} is found, a 'dummy', already-closed context is returned.
-     *
-     * @return The context with the current ServletRequest.
-     * The context itself is non-<code>null</code>, but may contain a <code>null</code> value.
-     * @see #getValue()
-     */
     static Context<ServletRequest> current() {
-        final ServletRequestContext current = CONTEXT.get();
-        return current != null ? current : None.INSTANCE;
+        return CONTEXT.get();
     }
 
-    /**
-     * {@link #close()} restores the previous context, but clear() unconditionally removes the active context.
-     * <p>
-     * This is useful for boundary filters, whose Threads may be returned to some threadpool.
-     */
-    static void clear() {
-        CONTEXT.remove();
-        LOGGER.finest("Cleared ServletRequestContext stack.");
+    public ServletRequest getValue() {
+        return servletRequest;
     }
 
-    /**
-     * Dummy context containing no servlet request.
-     */
-    private static final class None implements Context<ServletRequest> {
-        private static final None INSTANCE = new None();
-
-        public ServletRequest getValue() {
-            return null;
-        }
-
-        public void close() {
-        }
-
-        @Override
-        public String toString() {
-            return "ServletRequestContext.None";
-        }
+    public void close() {
+        servletRequest = null;
+        CONTEXT.set(null);
     }
+
 }
---
+++ servletrequest-propagation/src/test/java/nl/talsmasoftware/context/servletrequest/ServletRequestContextFilterTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2016-2018 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.context.servletrequest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+public class ServletRequestContextFilterTest {
+
+    ServletRequestContextFilter subject;
+
+    ServletRequest mockRequest;
+    ServletResponse mockResponse;
+    FilterConfig mockConfig;
+
+    @Before
+    public void setUp() {
+        mockRequest = mock(ServletRequest.class);
+        mockResponse = mock(ServletResponse.class);
+        mockConfig = mock(FilterConfig.class);
+
+        subject = new ServletRequestContextFilter();
+        subject.init(mockConfig);
+    }
+
+    @After
+    public void tearDown() {
+        subject.destroy();
+        verifyNoMoreInteractions(mockRequest, mockResponse, mockConfig);
+    }
+
+    @Test
+    public void testToString() {
+        assertThat(subject, hasToString(ServletRequestContextFilter.class.getSimpleName()));
+    }
+
+    @Test
+    public void testSimpleFiltering() throws IOException, ServletException {
+        when(mockRequest.isAsyncStarted()).thenReturn(false);
+
+        subject.doFilter(mockRequest, mockResponse, new FilterChain() {
+            public void doFilter(ServletRequest request, ServletResponse response) {
+                assertThat(ServletRequestContextManager.currentServletRequest(), is(sameInstance(request)));
+            }
+        });
+
+        verify(mockRequest).isAsyncStarted();
+    }
+
+    @Test
+    public void testAsyncFiltering() throws IOException, ServletException {
+        AsyncContext mockAsyncContext = mock(AsyncContext.class);
+        when(mockRequest.isAsyncStarted()).thenReturn(true);
+        when(mockRequest.getAsyncContext()).thenReturn(mockAsyncContext);
+
+        subject.doFilter(mockRequest, mockResponse, new FilterChain() {
+            public void doFilter(ServletRequest request, ServletResponse response) {
+                assertThat(ServletRequestContextManager.currentServletRequest(), is(sameInstance(request)));
+            }
+        });
+
+        verify(mockRequest).isAsyncStarted();
+        verify(mockRequest).getAsyncContext();
+        verify(mockAsyncContext).addListener(any(ServletRequestContextAsyncListener.class));
+        verifyNoMoreInteractions(mockAsyncContext);
+    }
+
+    @Test
+    public void testAsyncFiltering_exceptionInAddListener() throws IOException, ServletException {
+        AsyncContext mockAsyncContext = mock(AsyncContext.class);
+        when(mockRequest.isAsyncStarted()).thenReturn(true);
+        when(mockRequest.getAsyncContext()).thenReturn(mockAsyncContext);
+        doThrow(IllegalStateException.class).when(mockAsyncContext).addListener(any(ServletRequestContextAsyncListener.class));
+
+        subject.doFilter(mockRequest, mockResponse, new FilterChain() {
+            public void doFilter(ServletRequest request, ServletResponse response) {
+                assertThat(ServletRequestContextManager.currentServletRequest(), is(sameInstance(request)));
+            }
+        });
+
+        verify(mockRequest).isAsyncStarted();
+        verify(mockRequest).getAsyncContext();
+        verify(mockAsyncContext).addListener(any(ServletRequestContextAsyncListener.class));
+        verifyNoMoreInteractions(mockAsyncContext);
+    }
+}
