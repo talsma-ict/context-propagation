ba6b471d8b934fb3abca4ae719a18e03dad33b11
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2019-03-05 08:54:42 +0100

    Add more package javadoc
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

--- context-propagation-java5/src/main/java/nl/talsmasoftware/context/package-info.java
+++ context-propagation-java5/src/main/java/nl/talsmasoftware/context/package-info.java
@@ -17,24 +17,35 @@
 /**
  * Main package defining the core {@code context-propagation} concepts in this library
  *
- * <h2><a href="Context.html">Context</a></h2>
+ * <h2>{@linkplain nl.talsmasoftware.context.Context}</h2>
  * <p>
- * Defines a context as something with a value that can be closed.
+ * A {@linkplain nl.talsmasoftware.context.Context context} contains
+ * a {@linkplain nl.talsmasoftware.context.Context#getValue() value}.<br>
+ * There can be one active context per thread. A context remains active until it is closed or another context
+ * is activated in that thread.
  *
- * <h2><a href="ContextManager.html">ContextManager</a></h2>
  * <p>
- * Interface defining required operations to manage contexts:
- * creating new contexts and obtaining 'current' contexts.
+ * An {@linkplain nl.talsmasoftware.context.threadlocal.AbstractThreadLocalContext AbstractThreadLocalContext}
+ * base class is provided that supports nested contexts and provides predictable behaviour for out-of-order closing.
  *
- * <h2><a href="ContextSnapshot.html">ContextSnapshot</a></h2>
+ * <h2>{@linkplain nl.talsmasoftware.context.ContextManager}</h2>
  * <p>
- * A snapshot of one or more active contexts
- * that can be reactivated temporarily (until closed).
+ * Manages the active context.
+ * Can {@linkplain nl.talsmasoftware.context.ContextManager#initializeNewContext(java.lang.Object) initialize a new context}
+ * and provides access to the {@linkplain nl.talsmasoftware.context.ContextManager#getActiveContext() active context}.
  *
- * <h2><a href="ContextManagers.html">ContextManagers</a></h2>
+ * <h2>{@linkplain nl.talsmasoftware.context.ContextSnapshot}</h2>
  * <p>
- * Contains a static method to create a new snapshot
- * from registered ContextManager implementations.
+ * A snapshot contains the current value from all known context managers.<br>
+ * These values can be reactivated in another thread.<br>
+ * Reactivated snapshots must be closed to avoid leaking context.
+ *
+ * <p>
+ * All context aware utility classes in this library are tested to make sure they reactivate and close snapshots in a safe way.
+ *
+ * <h2>{@linkplain nl.talsmasoftware.context.ContextManagers}</h2>
+ * <p>
+ * Utility class that detects available context managers and lets you take a snapshot of all active contexts at once.
  *
  * @author Sjoerd Talsma
  */
