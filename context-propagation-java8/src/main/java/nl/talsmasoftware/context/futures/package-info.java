7e87f02de76fb8e44e6c10c9dfedd73c14ed6e31
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2019-03-28 08:52:40 +0100

    Add javadoc to the java 8 futures package

---
+++ context-propagation-java8/src/main/java/nl/talsmasoftware/context/futures/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2016-2019 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Package for context-aware java {@link java.util.concurrent.Future} code.
+ *
+ * <h2>{@linkplain nl.talsmasoftware.context.futures.ContextAwareCompletableFuture}</h2>
+ * <p>
+ * A {@linkplain java.util.concurrent.CompletableFuture} that applies a certain
+ * {@linkplain nl.talsmasoftware.context.ContextSnapshot} to all
+ * {@linkplain java.util.concurrent.CompletionStage completion stages} of the completable future.
+ */
+package nl.talsmasoftware.context.futures;
