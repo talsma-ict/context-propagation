5eebbf37ac0ae06ab82b1a530a42b91016d7f855
Sjoerd Talsma <talsma.s@tkppensioen.nl>
2019-04-08 09:52:29 +0200

    Add locale context package javadoc
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

---
+++ locale-context/src/main/java/nl/talsmasoftware/context/locale/package-info.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016-2019 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Maintain a current {@linkplain java.util.Locale Locale} context.
+ *
+ * <p>
+ * Setting a current locale can be done using the {@linkplain nl.talsmasoftware.context.locale.LocaleContextManager}:
+ * <pre>{@code
+ * private static LocaleContextManager localeContextManager = new LocaleContextManager();
+ *
+ * private void runWithLocale(Locale locale, Runnable someCode) {
+ *     try (Context<Locale> ctx = localeContextManager.initializeNewContext(locale)) {
+ *         someCode.run();
+ *     }
+ * }
+ * }</pre>
+ *
+ * <p>
+ * Obtaining the current locale works similar:
+ * <pre>{@code
+ * private void someCode() {
+ *     Locale currentLocale = LocaleContextManager.getCurrentLocaleOrDefault();
+ *     // ...
+ * }
+ * }</pre>
+ */
+package nl.talsmasoftware.context.locale;
