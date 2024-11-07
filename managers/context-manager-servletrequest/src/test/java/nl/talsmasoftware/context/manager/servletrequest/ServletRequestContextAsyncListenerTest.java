dd319c2b3038c627aef1553368ccec590b7a3fd9
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2024-11-07 18:32:39 +0100

    Organization: Move ServletRequest Context Manager in 'managers' group.
    Signed-off-by: Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>

--- managers/context-manager-servletrequest/src/test/java/nl/talsmasoftware/context/manager/servletrequest/ServletRequestContextAsyncListenerTest.java
+++ managers/context-manager-servletrequest/src/test/java/nl/talsmasoftware/context/manager/servletrequest/ServletRequestContextAsyncListenerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.talsmasoftware.context.servletrequest;
+package nl.talsmasoftware.context.manager.servletrequest;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
--- managers/context-manager-servletrequest/src/test/java/nl/talsmasoftware/context/manager/servletrequest/ServletRequestContextManagerTest.java
+++ managers/context-manager-servletrequest/src/test/java/nl/talsmasoftware/context/manager/servletrequest/ServletRequestContextManagerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.talsmasoftware.context.servletrequest;
+package nl.talsmasoftware.context.manager.servletrequest;
 
 import nl.talsmasoftware.context.api.Context;
 import nl.talsmasoftware.context.core.ContextManagers;
