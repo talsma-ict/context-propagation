7de104643551e87dd90c73363b0aaa3593e1d553
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2024-11-19 20:28:21 +0100

    Refactor package of ServletRequestContextManager.
    Signed-off-by: Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>

--- managers/context-manager-servletrequest/src/main/java/nl/talsmasoftware/context/managers/servletrequest/ServletRequestContextFilter.java
+++ managers/context-manager-servletrequest/src/main/java/nl/talsmasoftware/context/managers/servletrequest/ServletRequestContextFilter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.talsmasoftware.context.manager.servletrequest;
+package nl.talsmasoftware.context.managers.servletrequest;
 
 import nl.talsmasoftware.context.api.Context;
 
