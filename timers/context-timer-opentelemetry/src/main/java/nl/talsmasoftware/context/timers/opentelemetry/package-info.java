59fcc3d366d86059b763a3c40bfaa97063a7bb42
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2024-11-24 15:52:14 +0100

    Add javadoc for the opentelemetry context timer.
    Signed-off-by: Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>

---
+++ timers/context-timer-opentelemetry/src/main/java/nl/talsmasoftware/context/timers/opentelemetry/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2016-2024 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * {@link nl.talsmasoftware.context.api.ContextTimer Context timer} that
+ * creates a {@linkplain io.opentelemetry.api.trace.Span Span}
+ * using the {@linkplain io.opentelemetry.api.GlobalOpenTelemetry GlobalOpenTelemetry}
+ * for context switches.
+ *
+ * <p>
+ * Individual {@linkplain nl.talsmasoftware.context.api.ContextManager context managers}
+ * are <strong>not</strong> traced, only the operations regarding {@linkplain nl.talsmasoftware.context.api.ContextSnapshot}
+ * are traced.
+ */
+package nl.talsmasoftware.context.timers.opentelemetry;
+
