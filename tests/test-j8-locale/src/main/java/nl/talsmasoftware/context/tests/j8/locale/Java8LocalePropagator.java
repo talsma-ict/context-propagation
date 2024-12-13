9eab2e69cfd41c2f0e1c675b651a5715c0a05a80
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2024-12-13 17:08:23 +0100

    Exclude unused maven plugins from build and add simple smoketest for java 8 context propagation.
    Signed-off-by: Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>

---
+++ tests/test-j8-locale/src/main/java/nl/talsmasoftware/context/tests/j8/locale/Java8LocalePropagator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016-2024 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.context.tests.j8.locale;
+
+import nl.talsmasoftware.context.api.ContextSnapshot;
+import nl.talsmasoftware.context.api.ContextSnapshot.Reactivation;
+import nl.talsmasoftware.context.managers.locale.CurrentLocaleHolder;
+
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Java8LocalePropagator {
+
+    public static Locale getPropagatedLocaleFromNewThread() throws InterruptedException {
+        final ContextSnapshot snapshot = ContextSnapshot.capture();
+        final AtomicReference<Locale> result = new AtomicReference<>();
+        final Thread thread = new Thread(() -> {
+            try (Reactivation reactivation = snapshot.reactivate()) {
+                result.set(CurrentLocaleHolder.get().orElse(null));
+            }
+        });
+        thread.start();
+        thread.join(10000L);
+        return result.get();
+    }
+
+}
---
+++ tests/test-j8-locale/src/test/java/nl/talsmasoftware/context/tests/j8/locale/Java8LocalePropagatorTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016-2024 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.context.tests.j8.locale;
+
+import nl.talsmasoftware.context.managers.locale.CurrentLocaleHolder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Java8LocalePropagatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"nl-NL", "en-GB", "de-DE"})
+    void testLocalePropagation(String locale) throws InterruptedException {
+        // given
+        final Locale currentLocale = parseLocale(locale);
+        CurrentLocaleHolder.set(currentLocale);
+
+        // when
+        final Locale result = Java8LocalePropagator.getPropagatedLocaleFromNewThread();
+
+        // then
+        assertThat(result).isEqualTo(currentLocale);
+    }
+
+    private static Locale parseLocale(String locale) {
+        return Locale.forLanguageTag(locale.replace("_", "-"));
+    }
+}
